58f992e7ee2a3d87375f198d9c1c06d03fa8248e
karthik007343 <159223934+karthik007343@users.noreply.github.com>
2024-02-12 07:08:34 +0530

    Create username.java

---
+++ username.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+ class hello
+{
+public static void main(String args[ ])
+{
+System.out.println(“enter your name : ”);
+Scanner input=new Scanner(System.in);
+String name=input.nextLine( );
+System.out.println(“hello” +name);
+}
+}
